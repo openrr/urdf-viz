680f981343af765657cad068cd496a3045b84e69
Takashi Ogura <t.ogura@gmail.com>
2017-10-08 10:30:02 +0900

    Update create_bin.sh to support other ubuntu version

--- create_bin.sh
+++ create_bin.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 BINARY=$(grep name Cargo.toml | cut -d \" -f 2 | head -n1)
-OS=ubuntu16.04
-ARCH=amd64
+OS=ubuntu"$(lsb_release -r | cut -f2)"
+ARCH=$(uname -m)
 OUT_FILE=${BINARY}-$(grep version Cargo.toml | cut -d \" -f 2 | head -n1)-${OS}-${ARCH}.tar.gz
+BUILD=false
+BIN_DIR=./target/release/
+
+if [[ "$1" == "-b" ]]; then
+    BUILD=true
+fi
 
 main() {
-#    cargo build --release
-    tar -C ./target/release/ -czvf ${OUT_FILE} ./urdf-viz
-    echo "${OUT_FILE} is generated"
+    if [[ "$BUILD" == "true" ]]; then
+        cargo build --release
+    fi
+    if [[ -d "$BIN_DIR" ]]; then
+        tar -C "$BIN_DIR" -czvf "${OUT_FILE}" ./urdf-viz
+        echo "${OUT_FILE} is generated"
+    else
+        echo "build at first, use -b option"
+    fi
 }
 
 main
