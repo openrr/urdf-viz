7df51caec527df8b2eb9014982407a0ba5cfdd59
Taiki Endo <te316e89@gmail.com>
2021-09-30 17:06:34 +0900

    Fix default of RobotOrigin

--- src/handle.rs
+++ src/handle.rs
@@ -7,10 +7,19 @@ pub struct JointNamesAndPositions {
     pub positions: Vec<f32>,
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone, Default)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct RobotOrigin {
     pub position: [f32; 3],
     pub quaternion: [f32; 4],
+}
+
+impl Default for RobotOrigin {
+    fn default() -> Self {
+        Self {
+            position: [0.0; 3],
+            quaternion: [0.0, 0.0, 0.0, 1.0],
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
