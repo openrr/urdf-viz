bd5a5ff4dfa5cf2e35e994629d9aab14b1721c07
Taiki Endo <te316e89@gmail.com>
2022-05-16 18:33:13 +0900

    Switch to axum

--- src/web_server.rs
+++ src/web_server.rs
@@ -1,7 +1,12 @@
 use std::sync::Arc;
 
+use axum::{
+    extract::Extension,
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
-use warp::{Filter, Rejection};
 
 use crate::handle::{JointNamesAndPositions, RobotOrigin, RobotStateHandle};
 
@@ -11,6 +16,13 @@ type Handle = Arc<RobotStateHandle>;
 struct ResultResponse {
     is_ok: bool,
     reason: String,
+}
+
+impl ResultResponse {
+    const SUCCESS: Self = ResultResponse {
+        is_ok: true,
+        reason: String::new(),
+    };
 }
 
 #[derive(Debug)]
@@ -28,106 +40,64 @@ impl WebServer {
         self.handle.clone()
     }
 
-    #[tokio::main]
-    pub async fn start(self) {
-        let handle = self.handle();
+    pub async fn start_background(self) {
+        let app = app(self.handle());
 
-        let routes = set_joint_positions(handle.clone())
-            .or(set_robot_origin(handle.clone()))
-            .or(get_joint_positions(handle.clone()))
-            .or(get_robot_origin(handle.clone()))
-            .or(get_urdf_text(handle.clone()));
-        warp::serve(routes).run(([0, 0, 0, 0], self.port)).await;
+        let server =
+            axum::Server::bind(&([0, 0, 0, 0], self.port).into()).serve(app.into_make_service());
+        tokio::spawn(async move { server.await.unwrap() });
     }
 }
 
-fn set_joint_positions(
-    handle: Handle,
-) -> impl Filter<Extract = (warp::reply::Json,), Error = Rejection> + Clone {
-    warp::post()
-        .and(warp::path("set_joint_positions"))
-        .and(warp::body::json())
-        .and_then(move |jp: JointNamesAndPositions| {
-            let handle = handle.clone();
-            async move {
-                if jp.names.len() != jp.positions.len() {
-                    Ok::<_, Rejection>(warp::reply::json(&ResultResponse {
-                        is_ok: false,
-                        reason: format!(
-                            "names and positions size mismatch ({} != {})",
-                            jp.names.len(),
-                            jp.positions.len()
-                        ),
-                    }))
-                } else {
-                    handle.set_target_joint_positions(jp);
-                    Ok(warp::reply::json(&ResultResponse {
-                        is_ok: true,
-                        reason: "".to_string(),
-                    }))
-                }
-            }
-        })
+fn app(handle: Handle) -> Router {
+    Router::new()
+        .route("/set_joint_positions", post(set_joint_positions))
+        .route("/set_robot_origin", post(set_robot_origin))
+        .route("/get_joint_positions", get(get_joint_positions))
+        .route("/get_robot_origin", get(get_robot_origin))
+        .route("/get_urdf_text", get(get_urdf_text))
+        .layer(Extension(handle))
+        .layer(tower_http::trace::TraceLayer::new_for_http())
 }
 
-fn set_robot_origin(
-    handle: Handle,
-) -> impl Filter<Extract = (warp::reply::Json,), Error = Rejection> + Clone {
-    warp::post()
-        .and(warp::path("set_robot_origin"))
-        .and(warp::body::json())
-        .and_then(move |robot_origin: RobotOrigin| {
-            let handle = handle.clone();
-            async move {
-                handle.set_target_robot_origin(robot_origin);
-                Ok::<_, Rejection>(warp::reply::json(&ResultResponse {
-                    is_ok: true,
-                    reason: "".to_string(),
-                }))
-            }
+async fn set_joint_positions(
+    Json(jp): Json<JointNamesAndPositions>,
+    Extension(handle): Extension<Handle>,
+) -> Json<ResultResponse> {
+    if jp.names.len() != jp.positions.len() {
+        Json(ResultResponse {
+            is_ok: false,
+            reason: format!(
+                "names and positions size mismatch ({} != {})",
+                jp.names.len(),
+                jp.positions.len()
+            ),
         })
+    } else {
+        handle.set_target_joint_positions(jp);
+        Json(ResultResponse::SUCCESS)
+    }
 }
 
-fn get_joint_positions(
-    handle: Handle,
-) -> impl Filter<Extract = (warp::reply::Json,), Error = Rejection> + Clone {
-    warp::get()
-        .and(warp::path("get_joint_positions"))
-        .and_then(move || {
-            let handle = handle.clone();
-            async move {
-                let json = handle.current_joint_positions();
-                Ok::<_, Rejection>(warp::reply::json(&*json))
-            }
-        })
+async fn set_robot_origin(
+    Json(robot_origin): Json<RobotOrigin>,
+    Extension(handle): Extension<Handle>,
+) -> Json<ResultResponse> {
+    handle.set_target_robot_origin(robot_origin);
+    Json(ResultResponse::SUCCESS)
 }
 
-fn get_robot_origin(
-    handle: Handle,
-) -> impl Filter<Extract = (warp::reply::Json,), Error = Rejection> + Clone {
-    warp::get()
-        .and(warp::path("get_robot_origin"))
-        .and_then(move || {
-            let handle = handle.clone();
-            async move {
-                let robot_origin = handle.current_robot_origin();
-                Ok::<_, Rejection>(warp::reply::json(&*robot_origin))
-            }
-        })
+async fn get_joint_positions(Extension(handle): Extension<Handle>) -> Json<JointNamesAndPositions> {
+    Json(handle.current_joint_positions().clone())
 }
 
-fn get_urdf_text(
-    handle: Handle,
-) -> impl Filter<Extract = (warp::reply::Json,), Error = Rejection> + Clone {
-    warp::get()
-        .and(warp::path("get_urdf_text"))
-        .and_then(move || {
-            let handle = handle.clone();
-            async move {
-                match handle.urdf_text() {
-                    Some(text) => Ok(warp::reply::json(&*text)),
-                    None => Err(warp::reject::not_found()),
-                }
-            }
-        })
+async fn get_robot_origin(Extension(handle): Extension<Handle>) -> Json<RobotOrigin> {
+    Json(handle.current_robot_origin().clone())
+}
+
+async fn get_urdf_text(Extension(handle): Extension<Handle>) -> Result<String, StatusCode> {
+    match handle.urdf_text() {
+        Some(text) => Ok(text.clone()),
+        None => Err(StatusCode::NOT_FOUND),
+    }
 }
