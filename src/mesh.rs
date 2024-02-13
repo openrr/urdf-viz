7247c3a77edb8b317cd6eb9be13eb3f3085027cf
Taiki Endo <te316e89@gmail.com>
2024-02-13 18:02:22 +0900

    Do not panic on large mesh

--- src/mesh.rs
+++ src/mesh.rs
@@ -207,13 +207,8 @@ fn load_with_mesh_loader(
         let faces = mesh
             .faces
             .into_iter()
-            .map(|f| {
-                na::Point3::new(
-                    f[0].try_into().unwrap(),
-                    f[1].try_into().unwrap(),
-                    f[2].try_into().unwrap(),
-                )
-            })
+            // TODO: https://github.com/openrr/urdf-viz/issues/22
+            .map(|f| na::Point3::new(f[0] as u16, f[1] as u16, f[2] as u16))
             .collect();
 
         let kiss3d_mesh = Rc::new(RefCell::new(kiss3d::resource::Mesh::new(
